fbc804a4c886728b27cb02b5a8f8aa4f6c75bab1
igor-drozdov-n26 <igor.drozdov@n26.com>
2019-11-17 13:28:07 +0100

    Tomcat client

--- src/main/java/com/n26/kotlindemo/asyncdemo/service/DummyService.kt
+++ src/main/java/com/n26/kotlindemo/asyncdemo/service/DummyService.kt
@@ -4,6 +4,7 @@ import com.n26.kotlindemo.pojo.DataClass
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import kotlin.system.measureTimeMillis
 
 @Service
 class DummyService(
@@ -12,8 +13,12 @@ class DummyService(
 
     private val log = LoggerFactory.getLogger(DummyService::class.java)
 
-    fun makeRequest() =
-        restTemplate.getForEntity("http://localhost:8080", DataClass::class.java)
-            .body
+    fun makeRequest() {
+        val measureTimeMillis = measureTimeMillis {
+            restTemplate.getForEntity("http://localhost:8080/api/dummy", DataClass::class.java)
+                .body
+        }
+        log.info("time = {}", measureTimeMillis)
+    }
 
 }

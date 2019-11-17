6a1151bf42f73e9778107589b9e7881572a22c57
igor-drozdov-n26 <igor.drozdov@n26.com>
2019-11-17 13:43:45 +0100

    10 seconds cpu

--- src/main/java/com/n26/kotlindemo/asyncdemo/controller/DummyController.kt
+++ src/main/java/com/n26/kotlindemo/asyncdemo/controller/DummyController.kt
@@ -2,7 +2,6 @@ package com.n26.kotlindemo.asyncdemo.controller
 
 import com.n26.kotlindemo.asyncdemo.service.DummyService
 import com.n26.kotlindemo.pojo.DataClass
-import kotlinx.coroutines.delay
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,11 +13,18 @@ class DummyController(
 ) {
 
     @GetMapping
-    suspend fun getDummy(): DataClass {
-        delay(1000)
+    fun getDummy(): DataClass {
+        cpu(10)
         return DataClass("str", 10)
     }
 
     @GetMapping("send")
     fun makeRequest() = dummyService.makeRequest()
+
+    private fun cpu(milliseconds: Int) {
+        val sleepTime = milliseconds * 1000000L // convert to nanoseconds
+        val startTime = System.nanoTime()
+        while (System.nanoTime() - startTime < sleepTime) {
+        }
+    }
 }
